7d85c134b8fd979684f79e5ff97e1ac48eff1441
Smoking_Elk <andre12go@mail.ru>
2023-09-24 19:02:40 +0300

    fixed vectors.py... again

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ from vectors import Vec3
 def main():
     a = Vec3(1, 2, 1)
     b = Vec3(3, 5, -6)
-    c = Vec3(0, 0, 0)
+    c = a + b
 
     print(a, b, c)
+
+    aCopy = a.copy()
+    aCopy.x = 5
+    print(a, aCopy)
+
+    print(a.mag())
+
+    aCopy.norm()
+    print(aCopy.mag())
+
+    bCopy = b.copy()
+    print(b * 0.5)
+
+    v1 = Vec3(1, 0.5, 1)
+    v2 = Vec3(0.5, 0.5, 0)
+    v1 *= v2
+    print(v1)
+
 
 if __name__ == "__main__":
     main()
--- vectors.py
+++ vectors.py
@@ -19,6 +19,7 @@ class Vec3:
         self.x += other.x
         self.y += other.y
         self.z += other.z
+        return self
 
     def __sub__(self, other):
         return Vec3(
@@ -31,62 +32,65 @@ class Vec3:
         self.x -= other.x
         self.y -= other.y
         self.z -= other.z
+        return self
 
     def __mul__(self, other):
-        if instance(other, Vec3):
+        if isinstance(other, float):
             return Vec3(
-                self.x * other.x,
-                self.y * other.y,
-                self.z * other.z,
+                self.x * other,
+                self.y * other,
+                self.z * other,
             )
 
         return Vec3(
-            self.x * other,
-            self.y * other,
-            self.z * other,
+            self.x * other.x,
+            self.y * other.y,
+            self.z * other.z,
         )
 
     def __imul__(self, other):
-        if instance(other, Vec3): 
-            self.x *= other.x
-            self.y *= other.y
-            self.z *= other.z
-            return
+        if isinstance(other, float): 
+            self.x *= other
+            self.y *= other
+            self.z *= other
+            return self
 
         self.x *= other.x
         self.y *= other.y
         self.z *= other.z
+        return self
 
     def __truediv__(self, other):
-        if instance(other, Vec3):
+        if isinstance(other, float):
             return Vec3(
-                self.x / other.x,
-                self.y / other.y,
-                self.z / other.z,
+                self.x / other,
+                self.y / other,
+                self.z / other,
             )
 
         return Vec3(
-            self.x /other,
-            self.y / other,
-            self.z / other,
+            self.x / other.x,
+            self.y / other.y,
+            self.z / other.z,
         )
 
     def __itruediv__(self, other):
-        if instance(other, Vec3): 
-            self.x /= other.x
-            self.y /= other.y
-            self.z /= other.z
-            return
+        if isinstance(other, float): 
+            self.x /= other
+            self.y /= other
+            self.z /= other
+            return self
 
         self.x /= other.x
         self.y /= other.y
         self.z /= other.z
+        return self
 
-    def __len__(self):
+    def mag(self):
         return (self.x**2 + self.y**2 + self.z**2)**0.5
 
     def norm(self):
-        self /= len(self)
+        self /= self.mag()
         return self
 
     def mix(self, other, mixing):
