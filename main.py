7401db67c6ec1eeaf08a7657ba09ef02b4075f6f
Smoking_Elk <andre12go@mail.ru>
2023-09-24 19:22:06 +0300

    add code for drawing image using pseudographics

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from vectors import Vec3
 
+WIDTH = 120
+HEIGHT = 30
+ASPECT_RATIO = WIDTH / HEIGHT
+PIXEL_ASPECT = 11 / 24
+GRADIENT = " .:a|/(@"
 
+
+def getPixelColor(pixelX, pixelY):
+    x = (pixelX / WIDTH * 2 - 1) * ASPECT_RATIO * PIXEL_ASPECT
+    y = pixelY / HEIGHT * 2 - 1
+
+    if x**2 + y**2 < 0.5:
+        return 1
+    
+    return 0
+
+
+def calcImage():
+    res = []
+    COLORS_COUNT = len(GRADIENT)
+
+    for y in range(HEIGHT):
+        row = ""
+
+        for x in range(WIDTH):
+            brightness = getPixelColor(x, y)
+            color = GRADIENT[min(COLORS_COUNT - 1, int(brightness * COLORS_COUNT))]
+            row += color
+
+        res.append(row)
+
+    return "\n".join(res)
+
+    
 def main():
-    a = Vec3(1, 2, 1)
-    b = Vec3(3, 5, -6)
-    c = a + b
-
-    print(a, b, c)
-
-    aCopy = a.copy()
-    aCopy.x = 5
-    print(a, aCopy)
-
-    print(a.mag())
-
-    aCopy.norm()
-    print(aCopy.mag())
-
-    bCopy = b.copy()
-    print(b * 0.5)
-
-    v1 = Vec3(1, 0.5, 1)
-    v2 = Vec3(0.5, 0.5, 0)
-    v1 *= v2
-    print(v1)
+    image = calcImage()
+    print(image)    
 
 
 if __name__ == "__main__":
